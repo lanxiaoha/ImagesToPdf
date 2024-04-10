badaaa7e58082a318964d7512b5cbe6a92160e14
lanxiaoha <931763145@qq.com>
2024-04-10 14:40:55 +0800

    配置actions

--- pyinstallbuild.sh
+++ pyinstallbuild.sh
@@ -5,14 +5,15 @@
 #pip3 install pyinstaller
 #pip3 install PyQt5
 
-#pyinstaller --noconfirm \
-#-n ImageToPDF \
-#-i /Users/meitu/Documents/project/python/ImagesToPdf/logo.png \
-#--clean \
-#--windowed \
-#--specpath=./pyinstallbuild \
-#--onefile \
-#--noconsole \
-#src/main.py
+pyinstaller --noconfirm \
+-n ImageToPDF \
+-i /Users/meitu/Documents/project/python/ImagesToPdf/logo.png \
+--clean \
+--windowed \
+--specpath=./pyinstallbuild \
+--target-architecture=x86_64 \
+--onefile \
+--noconsole \
+src/main.py
 
-pyinstaller --noconfirm -n ImageToPDF --clean --windowed --specpath=./pyinstallbuild --onefile --noconsole src/main.py
+#pyinstaller --noconfirm -n ImageToPDF --clean --windowed --specpath=./pyinstallbuild --onefile --noconsole src/main.py
